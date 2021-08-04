0613f8405ecbe8554a0e2b8fd4074505f286c7c1
salocinrevenge <80754944+salocinrevenge@users.noreply.github.com>
2021-08-04 18:58:45 -0300

    resolvendo

--- src/jogoMago/Jogo.java
+++ src/jogoMago/Jogo.java
@@ -1,10 +1,39 @@
 package jogoMago;
 
-public class Jogo {
-	int altJanela=1080*3/5, largJanela=altJanela/9*16;
+import java.awt.Graphics;
+
+public class Jogo implements Runnable{
+	private int altJanela=1080*3/5, largJanela=altJanela/9*16;
+	private Janela janela = null;
+	private Graphics g = null;
+	private Thread thread;
+	private boolean running = false;
 	
 	public Jogo() {
-		new Janela(largJanela, altJanela);
+		this.janela = new Janela(largJanela, altJanela);
+		g = janela.getGraficos();
+		
+	}
+	
+	public synchronized void start(){
+		thread = new Thread(this);
+		thread.start();
+		running=true;
+	}
+	
+	public synchronized void stop(){
+		try{
+			thread.join();
+			running=false;
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void run() {
+		
 	}
 	
 }
