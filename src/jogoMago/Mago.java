6cd6ffa2b761f2b80c89ae4e62883a52e8b0bab8
MarryAngel <70298868+MarryAngel@users.noreply.github.com>
2021-08-05 15:06:03 -0300

    ataque epileptico curado

--- src/jogoMago/Mago.java
+++ src/jogoMago/Mago.java
@@ -1,26 +1,28 @@
 package jogoMago;
 
 public class Mago extends Objetos{
-	String tipo;
+	private int contador;
+	
+	
 	public Mago(int x, int y, int larg, int alt, String tipo, Manipulador manipulador){
 		this.x=x;
 		this.y=y;
 		this.largura=larg;
 		this.altura=alt;
-		this.tipo=tipo;
 		this.nome=tipo;
 		this.estado="Parado";
 		this.frame=0;
 		this.manipulador=manipulador;
-		
+		this.contador=0;
 		if(tipo=="Gelo")
 			this.invertido=true;
 	}
 	
 	public void tick() {
 		super.tick();
-		if(estado=="Parado")
+		if(estado=="Parado" && contador==0)
 			this.frame=FuncoesAuxiliares.ciclo(this.frame, 4);
+		this.contador=FuncoesAuxiliares.ciclo(this.contador, 10);
 	}
 	
 }
