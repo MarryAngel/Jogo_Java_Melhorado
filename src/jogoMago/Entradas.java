d11416265ee990d56aca6be2715a69f814bfb39a
MarryAngel <70298868+MarryAngel@users.noreply.github.com>
2021-08-06 18:55:55 -0300

    Alguem está viciado em mandar codigo pelo discord complicando a situação do git

--- src/jogoMago/Entradas.java
+++ src/jogoMago/Entradas.java
@@ -5,91 +5,94 @@ import java.awt.event.KeyEvent;
 import java.util.Arrays;
 
 public class Entradas extends KeyAdapter {
-	private Logica logica;
-	private boolean pressionados[] = new boolean[256];
-	
-	
-	public Entradas(Logica logica) {
-		this.logica=logica;
-		Arrays.fill(pressionados, false);
-	}
-	
+    private Logica logica;
+    private boolean pressionados[] = new boolean[256];
+    
+    
+    public Entradas(Logica logica) {
+        this.logica=logica;
+        Arrays.fill(pressionados, false);
+    }
+    
     public void keyPressed(KeyEvent e) {
-    	if(!pressionados[e.getKeyCode()]) {
+        if(!pressionados[e.getKeyCode()]) {
             if(e.getKeyCode() == KeyEvent.VK_D) {
-            	logica.clicar('D');
+                logica.clicar('D',true);
             }
             if(e.getKeyCode() == KeyEvent.VK_A) {
-            	logica.clicar('A');
+                logica.clicar('A',true);
             }
             if(e.getKeyCode() == KeyEvent.VK_W) {
-            	logica.clicar('W');
+                logica.clicar('W',true);
             }
             if(e.getKeyCode() == KeyEvent.VK_S) {
-            	logica.clicar('S');
+                logica.clicar('S',true);
             }
-            if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-            	logica.clicar(' ');
+            if(e.getKeyCode() == KeyEvent.VK_E) {
+                logica.clicar('E',true);
             }
             if(e.getKeyCode() == 39) {
-            	logica.clicar('d');
+                logica.clicar('d',true);
             }
             if(e.getKeyCode() == 37) {
-            	logica.clicar('a');
+                logica.clicar('a',true);
             }
             if(e.getKeyCode() == 38) {
-            	logica.clicar('w');
+                logica.clicar('w',true);
             }
             if(e.getKeyCode() == 40) {
-            	logica.clicar('s');
+                logica.clicar('s',true);
             }
             if(e.getKeyCode() == 16) {
-            	logica.clicar('c');
+                logica.clicar('c',true);
             }
             if(e.getKeyCode() == KeyEvent.VK_C) {
-            	logica.clicar('c');
+                logica.clicar('c',true);
             }
             if(e.getKeyCode() == KeyEvent.VK_J) {
-            	logica.clicar('j');
+                logica.clicar('j',true);
+            }
+            if(e.getKeyCode() == KeyEvent.VK_O) {
+                System.out.println(this.logica.getComandos(true));
             }
             pressionados[e.getKeyCode()]=true;
-    	}
+        }
     }
     
     public void keyReleased(KeyEvent e) {
-    	pressionados[e.getKeyCode()]=false;
+        pressionados[e.getKeyCode()]=false;
         if(e.getKeyCode() == KeyEvent.VK_D) {
-        	logica.soltar('D');
+            logica.soltar('D',true);
         }
         if(e.getKeyCode() == KeyEvent.VK_A) {
-        	logica.soltar('A');
+            logica.soltar('A',true);
         }
         if(e.getKeyCode() == KeyEvent.VK_W) {
-        	logica.soltar('W');
+            logica.soltar('W',true);
         }
         if(e.getKeyCode() == KeyEvent.VK_S) {
-        	logica.soltar('S');
+            logica.soltar('S',true);
         }
-        if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-        	logica.soltar(' ');
+        if(e.getKeyCode() == KeyEvent.VK_E) {
+            logica.soltar('E',true);
         }
         if(e.getKeyCode() == 39) {
-        	logica.soltar('d');
+            logica.soltar('d',true);
         }
         if(e.getKeyCode() == 37) {
-        	logica.soltar('a');
+            logica.soltar('a',true);
         }
         if(e.getKeyCode() == 38) {
-        	logica.soltar('w');
+            logica.soltar('w',true);
         }
         if(e.getKeyCode() == 40) {
-        	logica.soltar('s');
+            logica.soltar('s',true);
         }
         if(e.getKeyCode() == 16) {
-        	logica.soltar('c');
+            logica.soltar('c',true);
         }
         if(e.getKeyCode() == KeyEvent.VK_C) {
-        	logica.soltar('c');
+            logica.soltar('c',true);
         }
         
     }
